61513398bb4662f881e1a5ad57a3a6ff3e443d6a
Emma Laitinen <emlaitine@gmail.com>
2022-05-17 21:26:48 +0300

    Part 9: 1

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,14 @@
+const calculateBmi = (height: number, weight: number): String => {
+  const heightInM = height / 100;
+  const bmi: number = weight / (heightInM * heightInM);
+
+  if (bmi <= 18.4) {
+    return "Underweight";
+  } else if (bmi >= 25.0) {
+    return "Overweight";
+  } else {
+    return "Normal (healthy weight)";
+  }
+};
+
+console.log(calculateBmi(180, 74));
