6f3c423c8d775dde82db114c7d292daabb2c6ad1
Emma Laitinen <emlaitine@gmail.com>
2022-07-19 21:01:55 +0300

    Part 9: 17

---
+++ part9/patientor/src/PatientPage/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import axios from "axios";
+
+import { useParams } from "react-router-dom";
+import { apiBaseUrl } from "../constants";
+import { useStateValue } from "../state";
+import { Patient } from "../types";
+
+const PatientPage = () => {
+  const [{ patient }, dispatch] = useStateValue();
+
+  const { id } = useParams<{ id: string }>();
+
+  React.useEffect(() => {
+    if (id) {
+      void axios.get<void>(`${apiBaseUrl}/ping`);
+
+      const fetchPatient = async () => {
+        try {
+          const { data: patientFromApi } = await axios.get<Patient>(
+            `${apiBaseUrl}/patients/${id}`
+          );
+          dispatch({ type: "SET_PATIENT", payload: patientFromApi });
+        } catch (e) {
+          console.error(e);
+        }
+      };
+      void fetchPatient();
+    }
+  }, [dispatch, id]);
+
+  return (
+    <div>
+      <h1>{patient?.name}</h1>
+      <p>
+        ssn: {patient?.ssn}
+        <br />
+        gender: {patient?.gender}
+        <br />
+        occupation: {patient?.occupation}
+      </p>
+    </div>
+  );
+};
+
+export default PatientPage;
